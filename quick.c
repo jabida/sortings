0dc46dce17a68b887dda38398fe95573da5124ef
jabida <sk.jabida506@gmail.com>
2019-11-19 14:48:02 +0530

    quick

---
+++ quick.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+ 
+int split(int a[], int low, int n) 
+{ 
+int pivot = a[n],temp; 
+int i = (low - 1); 
+
+for (int j = low; j <= n- 1; j++) 
+{ 
+		 
+if (a[j] < pivot) 
+{ 
+  i++;  
+  temp=a[i];
+  a[i]=a[j];
+  a[j]=temp; 
+		
+}
+
+temp=a[i+1];
+a[i+1]=a[n];
+a[n]=temp;
+return(i+1);
+ 
+} 
+
+}
+
+
+void quick(int a[], int low, int n) 
+{ 
+int i,j;
+if (low < n) 
+{ 
+		
+int b = split(a, low, n);
+              		 
+}
+ 
+for(i=0;i<n+1;i++)
+
+printf("%d\n",a[i]);
+
+
+}
+
+
