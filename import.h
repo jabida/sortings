6dc285767448951ba56a390de209a327daf6038f
jabida <sk.jabida506@gmail.com>
2019-11-20 10:54:46 +0530

    head

--- import.h
+++ import.h
@@ -1,6 +1,5 @@
 extern void bubble(int a[],int n);
 extern void insertion(int a[], int n);
 extern void selection(int a[], int n);
-extern void quick(int a[],int low,int n);
-
-
+extern void quick(int a[], int low, int n);
+extern void merge(int a],int low,int n);
